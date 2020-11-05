d31b6ca331764f17055a62faf5469b67df219a0d
CoderCXF <18251716983@163.com>
2020-11-05 19:59:55 +0800

    temp commit

--- myhttpd0.c
+++ myhttpd0.c
@@ -50,6 +50,53 @@ int get_line(int sock, char *buf, int size)
 }
 
 /**
+ * @description: 通过文件名获取文件的类型
+ * @param {*}
+ * @return {*}
+ */
+const char *get_file_type(const char *name)
+{
+    char* dot;
+
+    // 自右向左查找‘.’字符, 如不存在返回NULL
+    dot = strrchr(name, '.');   
+    if (dot == NULL)
+        return "text/plain; charset=utf-8";
+    if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm") == 0)
+        return "text/html; charset=utf-8";
+    if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg") == 0)
+        return "image/jpeg";
+    if (strcmp(dot, ".gif") == 0)
+        return "image/gif";
+    if (strcmp(dot, ".png") == 0)
+        return "image/png";
+    if (strcmp(dot, ".css") == 0)
+        return "text/css";
+    if (strcmp(dot, ".au") == 0)
+        return "audio/basic";
+    if (strcmp( dot, ".wav" ) == 0)
+        return "audio/wav";
+    if (strcmp(dot, ".avi") == 0)
+        return "video/x-msvideo";
+    if (strcmp(dot, ".mov") == 0 || strcmp(dot, ".qt") == 0)
+        return "video/quicktime";
+    if (strcmp(dot, ".mpeg") == 0 || strcmp(dot, ".mpe") == 0)
+        return "video/mpeg";
+    if (strcmp(dot, ".vrml") == 0 || strcmp(dot, ".wrl") == 0)
+        return "model/vrml";
+    if (strcmp(dot, ".midi") == 0 || strcmp(dot, ".mid") == 0)
+        return "audio/midi";
+    if (strcmp(dot, ".mp3") == 0)
+        return "audio/mpeg";
+    if (strcmp(dot, ".ogg") == 0)
+        return "application/ogg";
+    if (strcmp(dot, ".pac") == 0)
+        return "application/x-ns-proxy-autoconfig";
+
+    return "text/plain; charset=utf-8";
+}
+
+/**
  * @description: 404 notfound  web
  * @param {*}
  * @return {*}
@@ -65,20 +112,50 @@ void notfound(const char *filename)
  * @param {*}
  * @return {*}
  */
-void unimplemented(int fd)
+void unimplemented()
 {
 
     return;
 }
+/**
+ * @description: send response headers
+ * @param {*}
+ * @return {*}
+ */
+void send_headers(int cfd, const char *fileName) {
+    /*juege file type by fileName*/
+    char buf[1024] = {0};
+    const char *file_type;
+    file_type = get_file_type(fileName);
+    strcpy(buf, "HTTP/1.1 200 OK\r\n");
+    send(cfd, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: %s\r\n", file_type);
+    send(cfd, buf, strlen(buf), 0);
+    strcpy(buf, "\r\n");
+    send(cfd, buf, strlen(buf), 0);
+    return ;
+}
 
+void send_resource(int cfd, const char *fileName) {
+    int fd = 0;
+    int n = 0;
+    char buf[1024];
+    if ((fd = open(fileName, O_RDONLY)) == -1) {
+        perror("open file error:");
+        /*how to deal*/
+    }
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        send(cfd, buf, n, 0);
+    }
+    return ;
+}
 /**
  * @description: Handle GET request
  * @param {*}
  * @return {*}
  */
-void handle_GET(const char *fileName)
+void handle_GET(int cfd, const char *fileName)
 {
-    char file_content[1024];
     struct stat file_stat;
     int ret = 0;
     if ((ret = stat(fileName, &file_stat)) == -1) {
@@ -88,9 +165,10 @@ void handle_GET(const char *fileName)
     /*regular file*/
     if (S_ISREG(file_stat.st_mode)) {
         /*reponse message : stat line*/
-
-
+        send_headers(cfd, fileName);
         /*reponse message : reponse head*/
+        send_resource(cfd, fileName);
+        /*reponse message : reponse body*/
     }
     return;
 }
@@ -144,6 +222,7 @@ void deal_accept(int lfd, int epfd)
 {
     int cfd = 0;
     int ret = 0;
+    char IPStr[1024];
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
     cfd = accept(lfd, (struct sockaddr *)&client_addr, &client_addr_len);
@@ -152,6 +231,7 @@ void deal_accept(int lfd, int epfd)
         perror("accept error");
         exit(-1);
     }
+    printf("client ip: %s, client port: %d\n", inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, IPStr, sizeof(IPStr)), ntohs(client_addr.sin_port));
     /*noblock setting for cfd*/
     int flag = fcntl(cfd, F_GETFL);
     flag |= O_NONBLOCK;
@@ -176,18 +256,16 @@ void parse_request(int cfd, int epfd)
     char line[1024];
     char method[16], fileName[256], protocol[16];
     struct stat file_stat;
-    int numbers = 0, ret = 0, len = 0;
+    int numbers = 0, len = 0;
     numbers = get_line(cfd, line, sizeof(line));
-    if (numbers == 0)
-    {
+    if (numbers == 0){
         printf("client close\n");
         epoll_ctl(epfd, EPOLL_CTL_DEL, cfd, NULL);
         close(cfd);
-    }
-    else
-    {
+    } else {
         sscanf(line, "%[^ ] %[^ ] %[^ ]", method, fileName, protocol);
         /*judge whether this file exist*/
+        printf("method:%s fileName:%s protocol:%s", method, fileName, protocol);
         while (1)
         {
             char request_head[1024];
@@ -199,11 +277,11 @@ void parse_request(int cfd, int epfd)
             // printf("%s", request_head);
         }
         if (strcasecmp(method, "GET") == 0) {
-            handle_GET(fileName);
+            handle_GET(cfd, fileName);
         } else if (strcasecmp(method, "POST") == 0) {
             handle_POST(fileName);
         } else {
-            unimplemented(fileName);
+            unimplemented(cfd);
             return ;
         }
     }
